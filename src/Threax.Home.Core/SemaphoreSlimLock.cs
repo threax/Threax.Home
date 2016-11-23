c8af6fc40a7de2e47f8cf9cd2b433cf12d0a1137
Andrew Piper <andrew@threax.com>
2016-11-23 08:55:56 -0500

    Started making hue api.

---
+++ src/Threax.Home.Core/SemaphoreSlimLock.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threax.Home.Core
+{
+    public class SemaphoreSlimLock : IDisposable
+    {
+        private SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+
+        public SemaphoreSlimLock()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            semaphore.Dispose();
+        }
+
+        public async Task Run(Action func)
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                func();
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        public async Task<T> Run<T>(Func<T> func)
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                return func();
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        public async Task<T> Run<T>(Func<Task<T>> func)
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                return await func();
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+
+        public async Task Run(Func<Task> func)
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                await func();
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+    }
+}
---
+++ src/Threax.Home.Hue/Services/SyncedHueClient.cs
@@ -0,0 +1,33 @@
+﻿using Q42.HueApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Threax.Home.Core;
+
+namespace Threax.Home.Hue.Services
+{
+    public class SyncedHueClient : IDisposable
+    {
+        private SemaphoreSlimLock locker = new SemaphoreSlimLock();
+        private HueClient client;
+
+        public SyncedHueClient(HueClient client)
+        {
+            this.client = client;
+        }
+
+        public void Dispose()
+        {
+            locker.Dispose();
+        }
+
+        public async Task SendCommandAsync(LightCommand command, IEnumerable<string> lightList = null)
+        {
+            await locker.Run(async () =>
+            {
+                await client.SendCommandAsync(command, lightList);
+            });
+        }
+    }
+}
