ca852f375b71bf9ddcf902fd455359e496c60a0a
Andrew Piper <andrew@threax.com>
2017-01-16 12:55:15 -0500

    Added a controller to talk to zwave directly.

---
+++ src/Threax.Home.ZWave/Models/CommandClassReportView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZWave.Channel;
+
+namespace Threax.Home.ZWave.Models
+{
+    public class CommandClassReportView
+    {
+        public CommandClass Class { get; set; }
+        public byte NodeId { get; internal set; }
+        public byte Version { get; set; }
+    }
+}
