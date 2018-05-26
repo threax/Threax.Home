9c07678f79cdc40494222dca874e6ceac5a258b0
Andrew Piper <andrew@threax.com>
2018-05-26 17:59:43 -0400

    Removed values.

---
+++ Threax.Home/ModelSchemas/Switch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Threax.AspNetCore.Models;
+
+namespace Threax.Home.ModelSchemas
+{
+    //[RequireAuthorization(typeof(Roles), nameof(Roles.EditValues))]
+    [PluralName("Switches")]
+    public abstract class Switch
+    {
+        [Required]
+        [MaxLength(450)]
+        public String Name { get; set; }
+    }
+}
