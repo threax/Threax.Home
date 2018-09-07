4fe30174e283ded57644df4dca9abf63edec84ec
Andrew Piper <andrew@threax.com>
2018-09-07 15:15:06 -0400

    Added more status outputs for the thermostat.

--- Threax.Home/Client/Libs/ThermostatController.ts
+++ Threax.Home/Client/Libs/ThermostatController.ts
@@ -1,6 +1,7 @@
 import * as controller from 'hr.controller';
 import * as client from 'clientlibs.ServiceClient';
 import * as lcycle from 'hr.widgets.MainLoadErrorLifecycle';
+import * as toggles from 'hr.toggles';
 
 export class ThermostatController {
     public static get InjectorArgs(): controller.DiFunction<any>[] {
@@ -13,12 +14,32 @@ export class ThermostatController {
     private tempForm: controller.IForm<client.ThermostatTempInput>;
     private lifecycle: lcycle.MainLoadErrorLifecycle;
 
+    private status: toggles.Group;
+    private heating: controller.OnOffToggle;
+    private cooling: controller.OnOffToggle;
+    private off: controller.OnOffToggle;
+
+    private fanStatus: toggles.Group;
+    private fanon: controller.OnOffToggle;
+    private fanoff: controller.OnOffToggle;
+
     constructor(bindings: controller.BindingCollection, private data: client.ThermostatResult) {
         this.currentTempView = bindings.getView("currentTemp");
         this.currentTempToggle = bindings.getToggle("currentTemp");
 
         this.tempForm = bindings.getForm("tempForm");
         this.lifecycle = new lcycle.MainLoadErrorLifecycle(bindings.getToggle("main"), bindings.getToggle("load"), bindings.getToggle("error"), true);
+
+
+        this.heating = bindings.getToggle("heating");
+        this.cooling = bindings.getToggle("cooling");
+        this.off = bindings.getToggle("off");
+        this.status = new toggles.Group(this.heating, this.cooling, this.off);
+        
+        this.fanon = bindings.getToggle("fanon");
+        this.fanoff = bindings.getToggle("fanoff");
+        this.fanStatus = new toggles.Group(this.fanon, this.fanoff);
+
         this.setup();
     }
 
@@ -52,9 +73,30 @@ export class ThermostatController {
 
     private syncData() {
         var currentData = Object.create(this.data.data);
-        currentData.isOff = currentData.mode === "Off";
+        currentData.isOff = this.data.data.mode === client.Mode.Off;
         this.tempForm.setData(currentData);
         this.currentTempView.setData(currentData);
+
+        switch (this.data.data.state) {
+            case client.State.Heating:
+                this.status.activate(this.heating);
+                break;
+            case client.State.Cooling:
+                this.status.activate(this.cooling);
+                break;
+            default:
+                this.status.activate(this.off);
+                break;
+        }
+
+        switch (this.data.data.fanState) {
+            case client.FanState.On:
+                this.fanStatus.activate(this.fanon);
+                break;
+            default:
+                this.fanStatus.activate(this.fanoff);
+                break;
+        }
         this.currentTempToggle.on();
     }
 }
