1e14a1ff93f1803f9ea727dfc125bf9fa5bc94b9
Andrew Piper <andrew@threax.com>
2018-12-03 16:06:37 -0500

    Started adding a simplified interface to libcec.

---
+++ libCecCore/CecManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace libCecCore
+{
+    public class CecManager : IDisposable
+    {
+        public CecManager()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        /// <summary>
+        /// Scan the CEC bus and display device info.
+        /// </summary>
+        public void Scan()
+        {
+
+        }
+
+        /// <summary>
+        /// Get the power status of the specified device.
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        public bool GetPower(int address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public String GetName(int address)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Set the device with the given address to on.
+        /// </summary>
+        public IEnumerable<DeviceInfo> SetOn(int address)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Set the device with the given address to standby or off.
+        /// </summary>
+        public void SetStandby(int address)
+        {
+
+        }
+
+        /// <summary>
+        /// Set the hdmi port number of the cec adapter.
+        /// </summary>
+        /// <param name="port"></param>
+        public void SetHdmiPort(int device, int port)
+        {
+
+        }
+
+        /// <summary>
+        /// Reconnect to the CEC adapter.
+        /// </summary>
+        public void Reconnect()
+        {
+
+        }
+    }
+}
