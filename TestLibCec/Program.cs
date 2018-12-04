45dbb05a9f69801bd4ca604b902bdad33e52dd63
Andrew Piper <andrew@threax.com>
2018-12-04 01:03:59 -0500

    Verify power functions work.

--- TestLibCec/Program.cs
+++ TestLibCec/Program.cs
@@ -17,6 +17,10 @@ namespace TestLibCec
                     Console.WriteLine($"Name: {cecManager.GetVendor(device)}");
                     Console.WriteLine($"Power: {cecManager.GetPower(device)}");
                 }
+                //cecManager.SetStandby(CecLogicalAddress.TV);
+                //Console.ReadKey();
+                //cecManager.SetOn(CecLogicalAddress.TV);
+
                 Console.ReadKey();
             }
         }
