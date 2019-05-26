618bf02174fd1d5e6d830afb39d92d894329b177
Andrew Piper <andrew@threax.com>
2019-05-26 13:05:11 -0400

    Moved to regular zwave4net. Refactored zwave switches, but still hardcoded.

--- Threax.Home.ZWave/Repository/ZWaveSwitchRepository.cs
+++ Threax.Home.ZWave/Repository/ZWaveSwitchRepository.cs
@@ -36,25 +36,87 @@ namespace Threax.Home.ZWave.Repository
             this.config = config;
         }
 
+        public async Task<TOut> Get(string bridge, string id)
+        {
+            var byteId = byte.Parse(id);
+            //Hardcoded, for now
+            switch (byteId)
+            {
+                case 2:
+                    return await GetSwitch3Switch(bridge, 2);
+                default:
+                    throw new FileNotFoundException();
+            }
+        }
+
+        public async Task Set(TIn setting)
+        {
+            switch (int.Parse(setting.Id))
+            {
+                case 2:
+                    await SetSwitch3Switch(setting);
+                    break;
+                default:
+                    throw new FileNotFoundException();
+            }
+        }
+
+        public async Task<IEnumerable<TOut>> List()
+        {
+            var switches = new List<TOut>();
+
+            foreach(var node in await zwave.GetNodes())
+            {
+                var protocolInfo = await node.GetProtocolInfo();
+                switch (protocolInfo.GenericType)
+                {
+                    case GenericType.SwitchMultiLevel:
+                    case GenericType.SwitchBinary:
+                        switches.Add(new TOut()
+                        {
+                            Id = node.NodeID.ToString(),
+                            Value = "",
+                            Bridge = config.ComPort,
+                            Subsystem = SubsystemName,
+                            Name = $"{SubsystemName} Node {node.NodeID}"
+                        });
+                        break;
+                }
+            }
+
+            return switches;
+        }
+
         private async Task<Basic> GetBasicCommand(byte nodeId)
         {
             return (await zwave.GetNodes()).First(n => n.NodeID == nodeId).GetCommandClass<Basic>();
         }
 
-        public async Task<TOut> Get(string bridge, string id)
+        private async Task SetSwitch3Switch(TIn setting)
         {
-            var intId = int.Parse(id);
-            //Hardcoded, for now
-            Basic com;
-            switch (intId)
+            var com = await GetBasicCommand(2);
+            switch (Enum.Parse(typeof(Switch3SwitchPosition), setting.Value))
             {
-                case 2:
-                    com = await GetBasicCommand(2);
+                case Switch3SwitchPosition.off:
+                    await com.Set(0);
+                    break;
+                case Switch3SwitchPosition.low:
+                    await com.Set(1);
+                    break;
+                case Switch3SwitchPosition.medium:
+                    await com.Set(50);
+                    break;
+                case Switch3SwitchPosition.high:
+                    await com.Set(99);
                     break;
                 default:
-                    throw new FileNotFoundException();
+                    throw new ErrorResultException($"{setting.Value} is not a valid position for {setting.Id}");
             }
+        }
 
+        private async Task<TOut> GetSwitch3Switch(String bridge, byte id)
+        {
+            var com = await GetBasicCommand(2);
             var b = (await com.Get()).Value;
             Switch3SwitchPosition value = Switch3SwitchPosition.off;
             if (b == 0)
@@ -73,54 +135,15 @@ namespace Threax.Home.ZWave.Repository
             {
                 value = Switch3SwitchPosition.high;
             }
+
             return new TOut()
             {
-                Id = intId.ToString(),
+                Id = id.ToString(),
                 Value = value.ToString(),
                 Bridge = bridge,
                 Subsystem = SubsystemName,
-                Name = "Bedroom Fan"
+                Name = $"{SubsystemName} Node {com.Node.NodeID}"
             };
-        }
-
-        public async Task<IEnumerable<TOut>> List()
-        {
-            var switches = new List<TOut>();
-
-            switches.Add(await Get(config.ComPort, "2"));
-
-            return switches;
-        }
-
-        public async Task Set(TIn setting)
-        {
-            Basic com;
-            switch (int.Parse(setting.Id))
-            {
-                case 2:
-                    com = await GetBasicCommand(2);
-                    break;
-                default:
-                    throw new FileNotFoundException();
-            }
-
-            switch (Enum.Parse(typeof(Switch3SwitchPosition), setting.Value))
-            {
-                case Switch3SwitchPosition.off:
-                    await com.Set(0);
-                    break;
-                case Switch3SwitchPosition.low:
-                    await com.Set(1);
-                    break;
-                case Switch3SwitchPosition.medium:
-                    await com.Set(50);
-                    break;
-                case Switch3SwitchPosition.high:
-                    await com.Set(99);
-                    break;
-                default:
-                    throw new ErrorResultException($"{setting.Value} is not a valid position for {setting.Id}");
-            }
         }
     }
 }
