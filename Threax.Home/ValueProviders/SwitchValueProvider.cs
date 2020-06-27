a4535f7e6d5c14a1ca5cf5d6b5a820db145fd037
Andrew Piper <andrew@threax.com>
2020-06-27 08:27:38 -0400

    Updated nuget and npm packages.

--- Threax.Home/ValueProviders/SwitchValueProvider.cs
+++ Threax.Home/ValueProviders/SwitchValueProvider.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using NJsonSchema;
 using Threax.AspNetCore.Halcyon.Ext.ValueProviders;
 
 namespace Threax.Home.ValueProviders
