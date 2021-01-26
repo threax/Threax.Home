ac63ef4f1ace80e83d39cbae8a881b6f8d359161
Andrew Piper <andrew@threax.com>
2021-01-26 17:18:10 -0500

    Updated ZWave4Net version.

--- Push.ps1
+++ Push.ps1
@@ -1,4 +1,4 @@
-git push --all
-git push https://github.com/threax/Threax.Home.git --all
-git push https://gitlab.com/threax-software/threax.home.git --all
-git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.Home --all
+git push
+git push https://github.com/threax/Threax.Home.git
+git push https://gitlab.com/threax-software/threax.home.git
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.Home
