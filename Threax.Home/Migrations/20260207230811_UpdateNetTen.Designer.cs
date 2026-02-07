8fdf4d7bb5ef72654e366889fea5a0d418a5808e
Andrew Piper <andrew@threax.com>
2026-02-07 18:10:55 -0500

    Add a migration for net10.

--- Threax.Home/Migrations/20260207230811_UpdateNetTen.Designer.cs
+++ Threax.Home/Migrations/20260207230811_UpdateNetTen.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Threax.Home.Database;
 
@@ -10,9 +11,11 @@ using Threax.Home.Database;
 namespace Threax.Home.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260207230811_UpdateNetTen")]
+    partial class UpdateNetTen
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.2");
---
+++ Threax.Home/Migrations/20260207230811_UpdateNetTen.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Threax.Home.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNetTen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Sensors",
+                type: "TEXT",
+                maxLength: 450,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 450);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Sensors",
+                type: "TEXT",
+                maxLength: 450,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 450,
+                oldNullable: true);
+        }
+    }
+}
