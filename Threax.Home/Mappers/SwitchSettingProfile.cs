62e9d8f3921281e346248af44c4186881607eeda
Andrew Piper <andrew@threax.com>
2026-04-30 19:11:20 -0400

    Check for null in switch setting.

--- Threax.Home/Mappers/SwitchSettingProfile.cs
+++ Threax.Home/Mappers/SwitchSettingProfile.cs
@@ -27,7 +27,7 @@ namespace Threax.Home.Mappers
         {
             dest.SwitchSettingId = src.SwitchId;
             dest.SwitchId = src.SwitchId;
-            dest.Switch = MapSwitch(src.Switch, new Switch());
+            dest.Switch = src.Switch != null ? MapSwitch(src.Switch, new Switch()) : null;
             dest.Value = src.Value;
             dest.Brightness = src.Brightness;
             dest.HexColor = src.HexColor;
